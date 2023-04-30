c37b8306835eece4c31c5d7e911a2ae78f460302
Tatiana0325 <krasnova-tatyana92@mail.ru>
2023-04-30 19:08:21 +0300

    Change index.js

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+
+
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    let timer = setInterval( () => {
+    let second = (seconds%60 < 10) ? `0${seconds%60}` : seconds%60; 
+    let minute = (seconds/60%60 < 10) ? `0${Math.trunc(seconds/60%60)}` : Math.trunc(seconds/60%60); 
+    let hour = (seconds/3600%60 < 10) ? `0${Math.trunc(seconds/3600%60)}` : Math.trunc(seconds/3600%60);
+                
+    if (seconds < 0) {
+      clearInterval(timer);
+    } else { 
+      timerEl.innerHTML = `${hour}:${minute}:${second}`;
+      };
+
+    seconds--;
+    }, 1000)  
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+  inputEl.value = inputEl.value.replace(/[^\d]/g, '');
 });
 
 buttonEl.addEventListener('click', () => {
